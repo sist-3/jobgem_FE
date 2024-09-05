1f3556179f98e013a4c27c5f5b008dc82b453232
KYBYK <dbqks56231106@gmail.com>
2024-09-05 22:15:41 +0900

    Feat : 이메일 유효성 검사 완료 시 이메일 인증 보낼 수 있는 상태

--- src/app/(FO)/reg/component/form/UserForm.js
+++ src/app/(FO)/reg/component/form/UserForm.js
@@ -1,6 +1,8 @@
+import { useFormContext } from "react-hook-form";
 import Input from "../Input";
 
 export default function UserForm() {
+    const { getFieldState, getValues } = useFormContext({name: "email"});
     const email_validation = {
         required: "이메일을 입력해주세요.",
         pattern: {
@@ -12,6 +14,14 @@ export default function UserForm() {
         required: "비밀번호를 입력해주세요.",
     }
 
+    const handleEmailCheck = () => {
+        const emailState = getFieldState("email");
+        const email = getValues("email");
+        if(!emailState.error && email) {
+            console.log("이메일 인증 가능")
+        }
+    }
+
     return (
         <div>
             <div className="flex">
@@ -19,7 +29,9 @@ export default function UserForm() {
                     <Input label="이메일" name="email" validation={email_validation} />
                 </div>
                 <div className="flex-1 w-1/5">
-                    <button className="w-full h-10 bg-blue-500 text-white rounded-md border">인증받기</button>
+                    <button type="button" className="w-full h-10 bg-blue-500 text-white rounded-md border hover:bg-blue-600"
+                    onClick={handleEmailCheck}
+                    >인증받기</button>
                 </div>
             </div>
             <Input type="password" label="비밀번호" name="password" validation={password_validation} />
